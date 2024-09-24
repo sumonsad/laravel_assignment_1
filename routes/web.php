62eec4827c76b00e6d6fdde1b77b6d50a07af39f
sumonsad <sumonsad22518@gmail.com>
2024-09-24 11:00:07 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
